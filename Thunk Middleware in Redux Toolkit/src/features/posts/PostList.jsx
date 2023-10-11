c24c3b23c96be728152ee02d895a51cd4ff97c90
Momin Naveed <mominnaveed398@gmail.com>
2023-10-11 17:10:21 +0500

    thunk middleware

---
+++ Thunk Middleware in Redux Toolkit/src/features/posts/PostList.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchPosts,
+  getPostsError,
+  getPostsStatus,
+  selectAllPosts,
+} from './postsSlice';
+import PostExcerpt from './PostExcerpt';
+import { fetchUsers } from '../users/usersSlice';
+
+const PostList = () => {
+  const dispatch = useDispatch();
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+  const posts = useSelector(selectAllPosts);
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchUsers());
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
+
+  // const orderedPosts = posts
+  //   .slice()
+  //   .sort((a, b) => b.date.localeCompare(a.date));
+
+  let content;
+  if (postStatus === 'loading') {
+    content = <p>Loading....</p>;
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post, i) => (
+      <PostExcerpt key={i} post={post} />
+    ));
+  } else if (postStatus === 'failed') {
+    content = <p>{error}</p>;
+  }
+  return (
+    <section>
+      <h2>Posts</h2>
+      {content}
+    </section>
+  );
+};
+
+export default PostList;
---
+++ Thunk Middleware in Redux Toolkit/src/features/posts/postsSlice.js
@@ -0,0 +1,122 @@
+import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
+import axios from 'axios';
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const initialState = {
+  posts: [],
+  status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await axios.get(POSTS_URL);
+  return response.data;
+});
+
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost);
+    return response.data;
+  }
+);
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    addPost: {
+      reducer(state, action) {
+        state.posts.push(action.payload);
+      },
+      prepare(title, body, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            body,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsDown: 0,
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.posts.find((post) => post.id === postId);
+      console.log('====>', existingPost);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        // Adding date and reactions
+        let min = 1;
+        const loadedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsDown: 0,
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          };
+          return post;
+        });
+
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(loadedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        const sortedPosts = state.posts.sort((a, b) => {
+          if (a.id > b.id) return 1;
+          if (a.id < b.id) return -1;
+          return 0;
+        });
+        action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+        // End fix for fake API post IDs
+
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsDown: 0,
+          thumbsUp: 0,
+          hooray: 0,
+          heart: 0,
+          rocket: 0,
+          eyes: 0,
+        };
+        // console.log(action.payload);
+        state.posts.push(action.payload);
+      });
+  },
+});
+
+export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
+
+export const { addPost, reactionAdded } = postsSlice.actions;
+export default postsSlice.reducer;
